fda7150a2bc191452bb30ad030eab94c91a4146f
Nicholas Zographos <nicholas.zographos@scripps.com>
2025-03-17 12:50:56 -0400

    Enhance QueryGrammar with new methods for column and table handling
    - Introduced columnize method to convert arrays of column names into a delimited string.
    - Added wrapTable and preWrapTable methods for improved table wrapping, including case sensitivity handling.
    - Implemented getValue and wrapSegments methods for better expression and segment processing.
    - Enhanced escape method for safe SQL value embedding.
    - Refactored wrapColumn to streamline column wrapping logic.

--- src/Flavours/Snowflake/Grammars/QueryGrammar.php
+++ src/Flavours/Snowflake/Grammars/QueryGrammar.php
@@ -1,10 +1,12 @@
 <?php
 namespace LaravelSnowflakeApi\Flavours\Snowflake\Grammars;
 
-use Illuminate\Database\Query\Builder;
-use Illuminate\Database\Query\Grammars\Grammar;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Schema\ColumnDefinition;
 
 class QueryGrammar extends Grammar
 {
@@ -40,29 +42,100 @@ class QueryGrammar extends Grammar
     }
 
     /**
-     * Wrap a single string in keyword identifiers.
+     * Convert an array of column names into a delimited string.
      *
-     * @param  string  $value
      * @return string
      */
-    // protected function wrapValue($value)
-    // {
-    //     if ('*' === $value) {
-    //         return $value;
-    //     }
+    public function columnize(array $columns)
+    {
+        return implode(', ', array_map([$this, 'wrapColumn'], $columns));
+    }
 
-    //     // If the value is actually a raw expression, we'll just return it as-is.
-    //     if (strpos($value, 'raw:') === 0) {
-    //         return substr($value, 4);
-    //     }
+    /**
+     * Wrap a table in keyword identifiers.
+     *
+     * @param \Illuminate\Database\Query\Expression|string $table
+     *
+     * @return string
+     */
+    public function wrapTable($table)
+    {
+        if (method_exists($this, 'isExpression') && !$this->isExpression($table)) {
+            $table = $this->preWrapTable($table);
+            return $this->wrap($this->tablePrefix . $table);
+        }
 
-    //     if (! env('SNOWFLAKE_COLUMNS_CASE_SENSITIVE', false)) {
-    //         $value = Str::upper($value);
-    //     }
+        return $this->getValue($table);
+    }
 
-    //     return '"' . str_replace('"', '""', $value) . '"';
-    // }
-        /**
+    public static function preWrapTable($tableName)
+    {
+        if ($tableName instanceof Blueprint) {
+            $tableName = $tableName->getTable();
+        }
+
+        if (! env('SNOWFLAKE_COLUMNS_CASE_SENSITIVE', false)) {
+            $tableName = Str::upper($tableName);
+        }
+
+        return $tableName;
+    }
+
+    /**
+     * Get the value of a raw expression.
+     *
+     * @param \Illuminate\Database\Query\Expression $expression
+     *
+     * @return string
+     */
+    public function getValue($expression)
+    {
+        return $expression instanceof Expression ? $expression->getValue($this) : $expression;
+    }
+
+    /**
+     * Wrap the given value segments.
+     *
+     * @param array $segments
+     *
+     * @return string
+     */
+    protected function wrapSegments($segments)
+    {
+        return collect($segments)->map(function ($segment, $key) use ($segments) {
+            return 0 === $key && count($segments) > 1
+                ? $this->wrapTable($segment)
+                // Original ->wraValue, but this is always called for columns segments
+                : $this->wrapColumn($segment);
+        })->implode('.');
+    }
+
+    /**
+     * Wrap a single string in keyword identifiers.
+     *
+     * @param string | \Illuminate\Database\Query\Expression $column
+     *
+     * @return string
+     */
+    protected function wrapColumn($column)
+    {
+
+        if (method_exists($this, 'isExpression') && $this->isExpression($column)) {
+            return $this->getValue($column);
+        }
+
+        if ($column instanceof ColumnDefinition) {
+            $column = $column->get('name');
+        }
+
+        if ('*' !== $column) {
+            return str_replace('"', '', $column);
+        }
+
+        return $column;
+    }
+
+    /**
      * Wrap a single string in keypublic function wrapTable($table)word identifiers.
      *
      * @param string $value
@@ -117,7 +190,6 @@ class QueryGrammar extends Grammar
         return $column;
     }
 
-    `
     /**
      * Compile the "offset" portions of the query.
      *
@@ -161,5 +233,30 @@ class QueryGrammar extends Grammar
         }
 
         return parent::wrapTable($table);
+    }
+
+        /**
+     * Wrap a union subquery in parentheses.
+     *
+     * @param string $sql
+     *
+     * @return string
+     */
+    protected function wrapUnion($sql)
+    {
+        return 'select * from ('.$sql.')';
+    }
+
+
+    /**
+     * Escapes a value for safe SQL embedding.
+     *
+     * @param  string|float|int|bool|null  $value
+     * @param  bool  $binary
+     * @return string
+     */
+    public function escape($value, $binary = false)
+    {
+        return DB::connection()->getPdo()->quote($value);
     }
 }
